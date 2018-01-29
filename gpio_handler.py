a8340dd86e4cf05fb5bee054e540374b85e5027a
Marc LeBlanc <thewhite147@gmail.com>
2018-01-28 22:21:30 -0500

    Game logic started

--- gpio_handler.py
+++ gpio_handler.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 import time
+from random import randint
+import math
 
 # GPIO Constants
 GPIO_INPUT_P1BA = 18 # Player 1 - Button A
@@ -18,6 +20,11 @@ STATE_GAME_OVER = 20
 
 # State of system
 system_state = STATE_MAIN_MENU
+serving_player = 0
+
+# Player's score
+score_player_1 = 0
+score_player_2  = 0
 
 # GPIO inputs configuration
 GPIO.setmode(GPIO.BCM)  
@@ -28,30 +35,69 @@ GPIO.setup(GPIO_INPUT_P2BB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(GPIO_INPUT_RESET, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def handle_button(button):
-    if system_state == STATE_MAIN_MENU # In main menu
-        if button == GPIO_INPUT_P1BA
+    if system_state == STATE_MAIN_MENU: # In main menu
+        if button == GPIO_INPUT_P1BA:
             #TODO Switch player 1
-        elif button == GPIO_INPUT_P1BB
+        elif button == GPIO_INPUT_P1BB:
             #TODO Select player 1
-        elif button == GPIO_INPUT_P2BA
+        elif button == GPIO_INPUT_P2BA:
             #TODO Switch player 2
-        elif button == GPIO_INPUT_P2BB
+        elif button == GPIO_INPUT_P2BB:
             #TODO Select player 2
 
-    elif system_state == STATE_IN_GAME # In game
-        if button == GPIO_INPUT_P1BA
+    elif system_state == STATE_IN_GAME: # In game
+        if button == GPIO_INPUT_P1BA:
             #TODO Increase player 1 score
-        elif button == GPIO_INPUT_P1BB
+        elif button == GPIO_INPUT_P1BB:
             #TODO Decrease player 1 score
-        elif button == GPIO_INPUT_P2BA
+        elif button == GPIO_INPUT_P2BA:
             #TODO Increase player 2 score
-        elif button == GPIO_INPUT_P2BB
+        elif button == GPIO_INPUT_P2BB:
             #TODO Decrease player 2 score
         else
             #TODO Go to main menu
 
-    elif system_state == STATE_GAME_OVER # After a game
+    elif system_state == STATE_GAME_OVER: # After a game
         #TODO Go to main menu
 
     else:
         pass
+
+
+def change_player(player):
+    #TODO Change player
+
+def select_player(player):
+    #TODO Select player
+
+def start_game():
+    system_state = STATE_IN_GAME
+    serving_player = randint(1, 2)
+    score_player_1 = 0
+    score_player_2 = 0
+    #TODO Send game to DB
+
+def change_score(player, diff):
+    if player == 1:
+        score_player_1 = score_player_1 + diff
+    elif player == 2:
+        score_player_2 = score_player_2 + diff
+
+    #TODO Change players score in DB
+
+    if score_player_1 >= 10 and score_player_2 >= 10: # Overtime handling
+        if math.fabs(score_player_1 - score_player_2) == 2
+            end_game()
+    elif score_player_1 == 11 or score_player_2 == 11: # End of the game
+        end_game()
+    
+
+def end_game():
+    system_state = STATE_GAME_OVER
+    #TODO End game in DB
+
+def main_menu():
+    system_state = STATE_MAIN_MENU
+    #TODO Set main menu in DB
+
+
