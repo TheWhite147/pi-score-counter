6a046e8467bd8f9ef472fd4ff1462f64f8147d24
Marc LeBlanc <thewhite147@gmail.com>
2018-02-06 21:19:31 -0500

    Added ID in get_players request

--- api/get_players.php
+++ api/get_players.php
@@ -3,11 +3,11 @@
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $pdo->prepare("SELECT name FROM players");
+    $stmt = $pdo->prepare("SELECT id, name FROM players");
     $stmt->execute();
     
     while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-        $data = $row[0] . "|";
+        $data = $row[0] . "=" . $row[1] . "|";
         print $data;
     }
 ?>
