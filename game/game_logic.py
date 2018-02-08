3733071727cb12fe889676dcffb15584834aec62
Marc LeBlanc <thewhite147@gmail.com>
2018-02-07 23:46:37 -0500

    Added more tests

--- game/game_logic.py
+++ game/game_logic.py
@@ -277,69 +277,6 @@ def handle_button(button):
         pass
 
 
-print("Tests will start in 1 sec...")
-time.sleep(1)
-
-print("TEST 1: Change player 1 two times")
-handle_button(GPIO_INPUT_P1BA)
-time.sleep(1)
-handle_button(GPIO_INPUT_P1BA)
-time.sleep(1)
-
-print("TEST 2: Changle player 2 four times, then set him ready")
-handle_button(GPIO_INPUT_P2BA)
-time.sleep(1)
-handle_button(GPIO_INPUT_P2BA)
-time.sleep(1)
-handle_button(GPIO_INPUT_P2BA)
-time.sleep(1)
-handle_button(GPIO_INPUT_P2BA)
-time.sleep(1)
-handle_button(GPIO_INPUT_P2BB)
-time.sleep(1)
-
-print("TEST 3: Set player 1 ready")
-handle_button(GPIO_INPUT_P1BB)
-time.sleep(1)
-
-print("TEST 4: Normal match")
-handle_button(GPIO_INPUT_P1BA) # 1-0
-time.sleep(2)
-handle_button(GPIO_INPUT_P2BA) # 1-1
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 2-1
-time.sleep(2)
-handle_button(GPIO_INPUT_P2BA) # 2-2
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 3-2
-time.sleep(2)
-handle_button(GPIO_INPUT_P2BA) # 3-3
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 4-3
-time.sleep(2)
-handle_button(GPIO_INPUT_P2BA) # 4-4
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 5-4
-time.sleep(2)
-handle_button(GPIO_INPUT_P2BA) # 5-5
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 6-5
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 7-5
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 8-5
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 9-5
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 10-5
-time.sleep(2)
-handle_button(GPIO_INPUT_P1BA) # 11-5 -> Game over!
-
-print("TEST 5: Wait 5 seconds then press any button to go back to main menu")
-time.sleep(5)
-handle_button(GPIO_INPUT_P1BA)
-
-
 # while True:
 #     input_state_P1BA = GPIO.input(GPIO_INPUT_P1BA)
 #     input_state_P1BB = GPIO.input(GPIO_INPUT_P1BB)
@@ -366,3 +303,225 @@ handle_button(GPIO_INPUT_P1BA)
 #     if input_state_RESET == False:
 #         handle_button(GPIO_INPUT_RESET)
 #         time.sleep(BUTTON_PRESS_DELAY)
+
+
+
+#########################
+########  Tests  ########
+#########################
+
+reset_all()
+
+print("Tests will start in 1 sec...")
+time.sleep(1)
+
+print("TEST 1: Change player 1 two times")
+handle_button(GPIO_INPUT_P1BA)
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA)
+time.sleep(1)
+
+print("TEST 2: Changle player 2 four times, then set him ready")
+handle_button(GPIO_INPUT_P2BA)
+time.sleep(0.5)
+handle_button(GPIO_INPUT_P2BA)
+time.sleep(0.5)
+handle_button(GPIO_INPUT_P2BA)
+time.sleep(0.5)
+handle_button(GPIO_INPUT_P2BA)
+time.sleep(0.5)
+
+handle_button(GPIO_INPUT_P2BB) # Player 2 ready
+time.sleep(1)
+
+print("TEST 3: Set player 1 ready")
+handle_button(GPIO_INPUT_P1BB)
+time.sleep(1)
+
+# print("TEST 4: Normal match")
+# handle_button(GPIO_INPUT_P1BA) # 1-0
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 1-1
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 2-1
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 2-2
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 3-2
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 3-3
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 4-3
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-4
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 5-4
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 5-5
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 6-5
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 7-5
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 8-5
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 9-5
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 10-5
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 11-5 -> Game over!
+
+# print("TEST 5: Wait 5 seconds then press any button to go back to main menu")
+# time.sleep(5)
+# handle_button(GPIO_INPUT_P1BA)
+
+# print("TEST 6: Player 1 won, so he's ready now! So player 2 will be another player. We will test if we can go back to index 0 when the selector comes at the end of the list")
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BB) # Player 1 ready
+
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA)
+
+# time.sleep(2)
+# handle_button(GPIO_INPUT_P2BB) # Player 2 ready, let's start again!
+
+# print("TEST 7: We will play a normal game, but we will decrease score sometime to test that feature")
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 1-0
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 2-0
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 3-0
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 4-0
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BB) # 3-0 -> Let's decrease!
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 4-0
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BA) # 5-0
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P1BB) # 4-0 -> Let's decrease!
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-1
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-2
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BB) # 4-1 -> Let's decrease!
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-2
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-3
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-4
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-5
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-6
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-7
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BB) # 4-6 -> Let's decrease!
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-7
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-8
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-9
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-10
+# time.sleep(1)
+# handle_button(GPIO_INPUT_P2BA) # 4-11 -> Game over!
+
+# print("TEST 8: Wait 5 seconds then press any button to go back to main menu")
+# time.sleep(5)
+# handle_button(GPIO_INPUT_P1BA)
+
+print("TEST 9: Overtime game")
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 1-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 2-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 3-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 4-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 5-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 6-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 7-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 8-0
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 9-0
+time.sleep(1)
+
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-1
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-2
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-3
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-4
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-5
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-6
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-7
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-8
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 9-9
+time.sleep(1)
+
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 10-9
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 10-10 -> Now in overtime
+
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 11-10
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 11-11
+
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 12-11
+time.sleep(1)
+handle_button(GPIO_INPUT_P2BA) # 12-12
+
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 13-12
+time.sleep(1)
+handle_button(GPIO_INPUT_P1BA) # 14-12 -> Game over!
+
+# print("TEST 10: Wait 5 seconds then press any button to go back to main menu")
+# time.sleep(5)
+# handle_button(GPIO_INPUT_P2BB)
+
+#print("TEST 9: Overtime with decreasing score")
+
+
