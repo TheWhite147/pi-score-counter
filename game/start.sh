35600e0d43b52652b29f295bb718dfc1dbc9dd1c
Marc LeBlanc <thewhite147@gmail.com>
2018-02-11 13:30:09 -0500

    Fixed start script

--- game/start.sh
+++ game/start.sh
@@ -4,4 +4,6 @@ clear
 echo -e "\e[92m** Game started! **\e[0m"
 
 chromium-browser file:///home/pi/pi-score-counter/web/index.html --start-fullscreen &
-sudo python3 ~/pi-score-counter/game_logic.py
+
+cd ~/pi-score-counter
+sudo python3 game_logic.py
