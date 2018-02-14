c9632969086f262c2bc812848168d04949adc07c
Marc LeBlanc <thewhite147@gmail.com>
2018-02-13 23:52:35 -0500

    Added variables

--- web/js/stats.js
+++ web/js/stats.js
@@ -1,5 +1,10 @@
 (function () {
 
+
+var _lstPlayers = [];
+var _lstScores = [];
+
+
 // Get initial banner stats then set interval for later updates
 updateBanner();
 setInterval(updateBanner, 1000 * 60 * 15) // 15 minutes - TODO: To adjust
@@ -8,11 +13,21 @@ setInterval(updateBanner, 1000 * 60 * 15) // 15 minutes - TODO: To adjust
 function updateBanner() {
 
     getBannerStats(function() {
-        Api.GetScores(function(data) {
-            
+        Api.GetScores(function(scores) {
+            _lstScores = scores;
+            Api.GetPlayersList(function(players) {
+                _lstPlayers = players;
+                
+                
+
+            });            
         });
     });
 
+}
+
+function findPlayer(id) {
+    _lstPlayers.find(function(el) { return el.id == id })
 }
 
 
