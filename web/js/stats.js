6f9ec398380434bcdff3acc746411db7fdf8f145
Marc LeBlanc <thewhite147@gmail.com>
2018-02-17 12:49:56 -0500

    Added stats

--- web/js/stats.js
+++ web/js/stats.js
@@ -28,17 +28,51 @@ function updateBanner() {
     getAllStatsData(function() {
 
         removeInvalidGames();
+        setGameInformationsFromScores();
         
         // Start by calculating stats for each players
         for (var i = 0; i < _lstPlayers.length; i++) {
 
-            // Find every valid games for this player
+            // Find every games for this player
             _lstPlayers[i].games = [];
 
+            // Statistics
+            _lstPlayers[i].games_played = 0;
+            _lstPlayers[i].games_won = 0;
+            _lstPlayers[i].games_lost = 0;
+            _lstPlayers[i].overtime_wins = 0;
+            _lstPlayers[i].overtime_losts = 0;
+            _lstPlayers[i].shutout_wins = 0;
+            _lstPlayers[i].shutout_losts = 0;
+            _lstPlayers[i].services_won = 0;
+            _lstPlayers[i].services_lost = 0;
+
             for (var j = 0; j < _lstGames.length; j++) {
-                if (isGameValid(_lstGames[j]) // Is the game valid?
-                && (_lstGames[j].id_player_1 == _lstPlayers[i].id || _lstGames[j].id_player_1 == _lstPlayers[i].id))
-                    return; //TODO
+                if (_lstGames[j].id_player_1 == _lstPlayers[i].id || _lstGames[j].id_player_1 == _lstPlayers[i].id) // Player played this game
+                    
+                    _lstPlayers[i].games_played++; // +1 game played
+
+                    if (_lstGames[j].id_winning_player == _lstPlayers[i].id) {
+                        _lstPlayers[i].games_won++; // +1 game won
+
+                        if (_lstGames[j].is_overtime)
+                            _lstPlayers[i].overtime_wins++; // +1 overtime won
+
+                        if (_lstGames[j].is_shuttout)
+                            _lstPlayers[i].shutout_wins++;
+
+                    }
+                    else {
+                        _lstPlayers[i].games_lost++; // +1 game lost
+
+                        if (_lstGames[j].is_overtime)
+                            _lstPlayers[i].overtime_losts++; // +1 overtime lost
+
+                        if (_lstGames[j].is_shuttout)
+                            _lstPlayers[i].shutout_losts++;
+
+                    }
+
             }
         }
     });
@@ -63,8 +97,33 @@ function getAllStatsData(callback) {
     });
 }
 
+function setGameInformationsFromScores() {
+    
+    for (var i = 0; i < _lstGames.length; i++) {
+        var sumScorePlayer1 = 0;
+        var sumScorePlayer2 = 0;
+    
+        var scores = findScores(_lstGames[i].id);
 
+        for (var j = 0; j < scores.length; j++) {
+            sumScorePlayer1 += scores[j].score_player_1;
+            sumScorePlayer2 += scores[j].score_player_2;
+        }
 
+        _lstGames[i].is_overtime = isOvertimeGame(sumScorePlayer1, sumScorePlayer2);
+        _lstGames[i].score_player_1 = sumScorePlayer1;
+        _lstGames[i].score_player_2 = sumScorePlayer2;
+
+        if (sumScorePlayer1 > sumScorePlayer2)
+            _lstGames[i].id_winning_player = _lstGames[i].id_player_1;
+        else 
+            _lstGames[i].id_winning_player = _lstGames[i].id_player_2;
+
+        if (sumScorePlayer1 == 0 || sumScorePlayer2 == 0)
+            _lstGames[i].is_shuttout = true;
+    }
+    
+}
 
 function removeInvalidGames() {
     for (var i = 0; i < _lstGames.length; i++) {
@@ -77,6 +136,7 @@ function removeInvalidGames() {
 
 function isGameValid(game) {
     var gameScores = findScores(game.id);
+    game.is_overtime = false;
 
     /* Is the game completed?
         - Sum of scores make 11 for a player
@@ -97,11 +157,18 @@ function isGameValid(game) {
     }
 
     // Valid overtime score (2 points of difference if both scores are > 10)
-    if (sumScorePlayer1 >= 10 && sumScorePlayer2 >= 10) { // Valid vertime game
+    if (isOvertimeGame(sumScorePlayer1, sumScorePlayer2)) { // Valid overtime game
             if (Math.abs(sumScorePlayer1 - sumScorePlayer2) == 2)
                 return true;
     } // Sum of scores make 11 for a player
     else if (sumScorePlayer1 == 11 || sumScorePlayer2 == 11) // Valid normal game
+        return true;
+
+    return false;
+}
+
+function isOvertimeGame(sumScorePlayer1, sumScorePlayer2) {
+    if (sumScorePlayer1 >= 10 && sumScorePlayer2 >= 10)
         return true;
 
     return false;
