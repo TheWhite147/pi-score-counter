b8829a3be85d70168c2b150c0b4a56e341e95aad
Marc LeBlanc <thewhite147@gmail.com>
2018-03-06 22:32:08 -0500

    Trying a fix to avoid false triggers

--- game/game_logic.py
+++ game/game_logic.py
@@ -3,6 +3,7 @@ import time
 from random import randint
 import math
 import dal
+import pigpio
 
 #######################
 ###  Configuration  ###
@@ -15,8 +16,12 @@ GPIO_INPUT_P2BA = 22 # Player 2 - Button A
 GPIO_INPUT_P2BB = 17 # Player 2 - Button B
 GPIO_INPUT_RESET = 25 # Reset button
 
+# pigpio instance
+pi = pigpio.pi()
+
 # Time Constants
-BUTTON_PRESS_DELAY = 0.2 # Delay between each button press
+BUTTON_PRESS_DELAY = 0.3 # Delay between each button press
+STEADY_SIGNAL_MICROSECONDS = 300000 # Microseconds needed to trigger a steady electrical signal (avoid false triggers)
 
 # Game Flow
 STATE_MAIN_MENU = 0
@@ -179,6 +184,13 @@ GPIO.setup(GPIO_INPUT_P1BB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(GPIO_INPUT_P2BA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(GPIO_INPUT_P2BB, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
 GPIO.setup(GPIO_INPUT_RESET, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Glitch filter to avoid false triggers
+pi.set_glitch_filter(GPIO_INPUT_P1BA, STEADY_SIGNAL_MICROSECONDS)
+pi.set_glitch_filter(GPIO_INPUT_P1BB, STEADY_SIGNAL_MICROSECONDS)
+pi.set_glitch_filter(GPIO_INPUT_P2BA, STEADY_SIGNAL_MICROSECONDS)
+pi.set_glitch_filter(GPIO_INPUT_P2BB, STEADY_SIGNAL_MICROSECONDS)
+pi.set_glitch_filter(GPIO_INPUT_RESET, STEADY_SIGNAL_MICROSECONDS)
 
 ########################
 ###  Game functions  ###
