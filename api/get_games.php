5f41c0532fd44a4aef4dbb22b8959882d6e69d25
Marc LeBlanc <thewhite147@gmail.com>
2018-03-20 13:25:45 -0400

    Added perf logs and WHERE clauses in API endpoints to score the last game only

--- api/get_games.php
+++ api/get_games.php
@@ -5,8 +5,14 @@
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $pdo->prepare("SELECT id, id_player_1, id_player_2, created_date FROM games");
-    $stmt->execute();
+    // To return only the games done after this one in parameters
+    $id_last_game = $_GET['id_last_game'];
+    if (empty($id_last_game)) {
+        $id_last_game = '0';
+    }
+
+    $stmt = $pdo->prepare("SELECT id, id_player_1, id_player_2, created_date FROM games WHERE id > ?");
+    $stmt->execute(array($id_last_game));
     
     while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
         $data = "id=" . $row[0] . "#id_player_1=" . $row[1] . "#id_player_2=" . $row[2] . "#created_date=" . $row[3] . "|";
