7180c9e11c48e533365bbe90fe1d2396f97ae6f2
Marc LeBlanc <thewhite147@gmail.com>
2018-03-31 00:26:35 -0400

    JS cleanup

--- web/js/modules/log.js
+++ web/js/modules/log.js
@@ -4,7 +4,7 @@ if (typeof Log.LogPerf === "undefined") { Log.LogPerf = {}; }
 (function () {
 
     Log.LogPerf = function(tag, time) {
-
+        
         if (time >= 1000) {
             console.warn("PERF - " + tag + ": " + time + " ms");
         } else {
