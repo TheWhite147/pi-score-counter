9c09d404c0259ce5a055c47eb4ffc829daac4a6a
Marc LeBlanc <thewhite147@gmail.com>
2019-12-03 18:47:14 -0500

    New seasons!

--- web/js/season.js
+++ web/js/season.js
@@ -38,10 +38,98 @@ Season.Message = Season.ActiveSeason.message;
 function createSeasons() {
 
     _lstSeasons.push({
-        startDate: new Date(2018, 10, 1).getTime() / 1000,
-        endDate: new Date(2019, 0, 1).getTime() / 1000,
-        name: "Saison 5 (novembre-décembre 2018)",
-        message: "La saison 5 (novembre-décembre 2018) est en cours"
+            startDate: new Date(2020, 10, 1).getTime() / 1000,
+            endDate: new Date(2021, 0, 1).getTime() / 1000,
+            name: "Saison 17 (novembre-décembre 2020)",
+            message: "La saison 17 (novembre-décembre 2020) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2020, 8, 1).getTime() / 1000,
+            endDate: new Date(2020, 10, 1).getTime() / 1000,
+            name: "Saison 16 (septembre-octobre 2020)",
+            message: "La saison 16 (septembre-octobre 2020) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2020, 6, 1).getTime() / 1000,
+            endDate: new Date(2020, 8, 1).getTime() / 1000,
+            name: "Saison 15 (juillet-août 2020)",
+            message: "La saison 15 (juillet-août 2020) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2020, 4, 1).getTime() / 1000,
+            endDate: new Date(2020, 6, 1).getTime() / 1000,
+            name: "Saison 14 (mai-juin 2020)",
+            message: "La saison 14 (mai-juin 2020) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2020, 2, 1).getTime() / 1000,
+            endDate: new Date(2020, 4, 1).getTime() / 1000,
+            name: "Saison 13 (mars-avril 2020)",
+            message: "La saison 13 (mars-avril 2020) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2020, 0, 1).getTime() / 1000,
+            endDate: new Date(2020, 2, 1).getTime() / 1000,
+            name: "Saison 12 (janvier-février 2020)",
+            message: "La saison 12 (janvier-février 2020) est en cours"
+    });  
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    _lstSeasons.push({
+            startDate: new Date(2019, 10, 1).getTime() / 1000,
+            endDate: new Date(2020, 0, 1).getTime() / 1000,
+            name: "Saison 11 (novembre-décembre 2019)",
+            message: "La saison 11 (novembre-décembre 2019) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2019, 8, 1).getTime() / 1000,
+            endDate: new Date(2019, 10, 1).getTime() / 1000,
+            name: "Saison 10 (septembre-octobre 2019)",
+            message: "La saison 10 (septembre-octobre 2019) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2019, 6, 1).getTime() / 1000,
+            endDate: new Date(2019, 8, 1).getTime() / 1000,
+            name: "Saison 9 (juillet-août 2019)",
+            message: "La saison 9 (juillet-août 2019) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2019, 4, 1).getTime() / 1000,
+            endDate: new Date(2019, 6, 1).getTime() / 1000,
+            name: "Saison 8 (mai-juin 2019)",
+            message: "La saison 8 (mai-juin 2019) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2019, 2, 1).getTime() / 1000,
+            endDate: new Date(2019, 4, 1).getTime() / 1000,
+            name: "Saison 7 (mars-avril 2019)",
+            message: "La saison 7 (mars-avril 2019) est en cours"
+    });
+
+    _lstSeasons.push({
+            startDate: new Date(2019, 0, 1).getTime() / 1000,
+            endDate: new Date(2019, 2, 1).getTime() / 1000,
+            name: "Saison 6 (janvier-février 2019)",
+            message: "La saison 6 (janvier-février 2019) est en cours"
+    });    
+
+    ////////////////////////////////////////////////////////////////////////////////////////
+
+    _lstSeasons.push({
+            startDate: new Date(2018, 10, 1).getTime() / 1000,
+            endDate: new Date(2019, 0, 1).getTime() / 1000,
+            name: "Saison 5 (novembre-décembre 2018)",
+            message: "La saison 5 (novembre-décembre 2018) est en cours"
     });
 
     _lstSeasons.push({
